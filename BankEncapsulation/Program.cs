a49b1ecc3a9aac7c962c8cea9f70baa708b8534d
nwhang1 <nicholasjwhang@gmail.com>
2024-04-03 12:41:19 -0700

    changed to have the BankAcocunt in a separate class

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,18 +1,5 @@
 ﻿namespace BankEncapsulation
 {
-
-    public class BankAccount
-    {
-        private double balance = 0;
-        public void Deposit(double amount)
-        {
-            balance += amount;
-        }
-        public double GetBalance()
-        {
-            return balance;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
