2b3af80531c0597eddf959c544fce73f6d6ea850
nwhang1 <nicholasjwhang@gmail.com>
2024-04-03 09:33:52 -0700

    Exercise complete

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace BankEncapsulation
 {
-    public class Program
+
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("Enter Deposit Amount");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(amount);
+
+            Console.WriteLine($"Balance : {account.GetBalance()}");
         }
     }
 }
